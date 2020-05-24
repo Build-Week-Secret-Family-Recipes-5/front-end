8117a2d100dbe06c643647bb816ada14db3dabdf
stephanie mairena <stefyem7711@gmail.com>
2020-05-24 02:46:00 -0400

    searchbarbugs

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+//import styled from "styled-components";
+
+
+function Header() {
+ const [results] = useState([
+
+
+ ])
+
+    return (
+        <h2>hello</h2>    )
+}
+
+export default Header;
